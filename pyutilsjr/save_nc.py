8e9c2640a269603f2ccc1f2e23fb568eac8bd63d
junior <francisco.vasconcelos@funceme.br>
2018-01-20 17:08:47 -0300

    add save_nc_echam46 module

--- pyutilsjr/save_nc.py
+++ pyutilsjr/save_nc.py
@@ -2,51 +2,45 @@
 
 __author__ = 'Marcelo Rodrigues'
 __email__ = 'marcelorodriguesss@gmail.com'
+__credits__ = 'Fco Vasconcelos Junior, Marcelo Rodrigues'
 __date__ = '07 Nov 2017'
+__modified__ = '20 Jan 2018'
 
 from netCDF4 import Dataset
 
+def save_nc_sst_echam46(sst, lat, lon, fname='sst_out.nc', year='1900'):
 
-def save_nc(var, lat, lon, fname='sst_out.nc', year='1900'):
+    foo = Dataset(fname, 'w', format='NETCDF3_CLASSIC')
 
-    foo = Dataset(fname, 'w', format='NETCDF4')
+    foo.createDimension('lat', 64)
+    foo.createDimension('lon', 128)
+    foo.createDimension('time', 12
 
-    foo.createDimension('Y', 181)
-    foo.createDimension('X', 360)
-    foo.createDimension('S', 12)
-    foo.createDimension('M', 24)
-    foo.createDimension('L', 10)
-
-    x = foo.createVariable('X', 'f8', ('X'))
+    x = foo.createVariable('lon', 'd', ('X'))
     x.units = 'degrees_E'
     x.long_name = 'longitude'
+    x.axis = 'X'
     x[:] = lon[:]
 
-    y = foo.createVariable('Y', 'f8', ('Y'))
+    y = foo.createVariable('lat', 'd', ('Y'))
     y.units = 'degrees_N'
     y.long_name = 'latitude'
+    y.axis = 'Y'
     y[:] = lat[:]
 
-    s = foo.createVariable('S', 'f8', ('S'))
-    s.units = 'months since {0}-01-15'.format(year)
-    s.long_name = "forecast start time"
-    s[:] = range(12)
+    l = foo.createVariable('time', 'i', ('time'))
+    l.units = 'months since {0}-01-15'.format(year)
+    l.calendar = '360_day'
+    l.long_name = "time"
+    l[:] = range(12)
 
-    m = foo.createVariable('M', 'f8', ('M'))
-    m.long_name = "member"
-    m[:] = range(24)
+    # L=12, Y=128, X=128
 
-    l = foo.createVariable('L', 'f8', ('L'))
-    l.long_name = "lead"
-    l[:] = range(10)
-
-    # S=12, L=10. M=24, Y=181, X=360
-
-    v = foo.createVariable('sst', float, ('S', 'L', 'M', 'Y', 'X'))
+    v = foo.createVariable('sst', float, ('time','lat', 'lon'))
     v.long_name = 'sea surface temperature'
-    v.units = '[C]'
-    # v.missing_value = -999
-    v[:] = var[:]
+    v.units = '[K]'
+    v.missing_value = -999
+    v[:] = sst[:]
 
     foo.close()
 
